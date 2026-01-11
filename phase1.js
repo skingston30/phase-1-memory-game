4c7a007f5e45f8bec2c3e41c2c93900c385b32e7
skingston30 <skingston91@gmail.com>
2026-01-11 13:44:35 -0500

    little bit of spice
    starting working on cards

--- phase1.js
+++ phase1.js
@@ -1,8 +1,15 @@
-document.addEventListener('DOMContentLoaded', function(e) {
-
+document.addEventListener('DOMContentLoaded', function (e) {
+    // dont forget the steps. one step at a time. 
     e.preventDefault();
+    // we are working on getting the cards to show up. we are starting with a div/continer method. see if we can get the card to show up. a table might also help to. look into both. 
+    function generateCards() {
 
-    function generateCards (){
-        const tbody = document.getElementById
+        const tbody = document.getElementById("game-container");
+
+        const cards1 = document.createElement('tr');
+        const cards2 = document.createElement('tr');
+        const cards3 = document.createElement('tr');
+
     }
+    generateCards();
 })
